aa893e8c3bb300caeab5ff340725ac55309e3af5
xzsyr <ixzsyr@163.com>
2018-06-08 08:53:36 +0800

    commit

--- craft-parent/craft-config-server/src/main/java/com/xzsyr/config/ConfigServerApplication.java
+++ craft-parent/craft-config-server/src/main/java/com/xzsyr/config/ConfigServerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
   
-/**        
+/**         
  * Title: ConfigServerApplication.java    
  * Description: 在分布式系统中，由于服务数量巨多，为了方便服务配置文件统一管理，实时更新，所以需要分布式配置中心组件；
  * 它支持配置服务放在配置服务的内存中（即本地），也支持放在远程Git仓库中。
