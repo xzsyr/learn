36c7970a36d9f56ebeefdf4f136b85f1f4f23d57
xzsyr <ixzsyr@163.com>
2018-06-19 09:13:18 +0800

    commit

--- met-parent/met-web/src/main/java/com/example/demo/controller/JobController.java
+++ met-parent/met-web/src/main/java/com/example/demo/controller/JobController.java
@@ -23,11 +23,9 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.alibaba.druid.util.StringUtils;
 import com.example.demo.dto.JobDTO;
 import com.example.demo.entity.Group;
 import com.example.demo.entity.JobAndTrigger;
